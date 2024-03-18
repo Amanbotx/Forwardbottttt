7351bf310f9da6a7e80b21fef6316b521946e826
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-18 21:39:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "d927c13beaaf5110f25c505b7c071273")
     BOT_TOKEN = environ.get("BOT_TOKEN", "6883904115:AAFzjOFjf30iab7YEh5dMQ5Y1D0V-6bBQ74") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.jfl45jq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.lw6mrgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "cluster0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5977931010').split()]
 
 class temp(object): 
